1ebd8794ef26e38540b444a1c3e4ba0855832296
chrisdownham <chris@r1se.co.uk>
2025-06-12 10:14:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/WlSdkAutoloader.php';
+require_once __DIR__ . '/WellnessLiving/wl-autoloader.php';
 WlSdkAutoloader::register();
 
 use WellnessLiving\Config\WlRegionSid;
