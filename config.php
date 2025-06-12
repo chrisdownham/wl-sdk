04349d82bea2891c52c672c03b541f549a77fd61
chrisdownham <chris@r1se.co.uk>
2025-06-12 18:35:30 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WlSdkExample;
+
+use WellnessLiving\Config\WlConfigDeveloper;
+
+/**
+ * The example config class.
+ */
+class ExampleConfig extends WlConfigDeveloper
+{
+  /**
+   * @inheritDoc
+   */
+  const AUTHORIZE_CODE = 'eBYCKvZ90FdqbLoRTb44tWOARpuZPLBaFphaSUZMOTh2';
+
+  /**
+   * @inheritDoc
+   */
+  const AUTHORIZE_ID = 'raa-EClh0AuHE8XbHWEO';
+}
